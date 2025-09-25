c5881acb8072b359ca423f73f603dd1f0f56ca86
Sri Lakshmi <srijish0609@gmail.com>
2025-09-25 21:44:30 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ai-driven-public-health-chatbot-for-disease-awareness
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup (
+    name='Medical AI Project',
+    version='0.0.0',
+    author='Techno Crew',
+    author_email='srijish0609@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
